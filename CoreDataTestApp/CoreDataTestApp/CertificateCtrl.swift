28ed092b95d8b0d6f95069b6173b479fdad5128c
RamAIDash <ram@aidash.com>
2020-04-04 15:27:13 +0530

    Certificate

---
+++ CoreDataTestApp/CoreDataTestApp/CertificateCtrl.swift
@@ -0,0 +1,30 @@
+//
+//  CertificateCtrl.swift
+//  CoreDataTestApp
+//
+//  Created by Ram Naresh on 4/4/20.
+//  Copyright © 2020 AIDASH. All rights reserved.
+//
+
+import UIKit
+
+class CertificateCtrl: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
